2a02077bb8264a6439f909ba7c5cfb45df892fb2
redrem95 <alexander.vollmer.95@hotmail.de>
2020-08-06 21:47:57 +0200

    added python provided version to docker versions added gunicorn timeout added submodule init for workflow

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-gunicorn -w ${WORKER_THREADS:-$(expr $(nproc --all) - 1)} -b 0.0.0.0:${BIND_ADDRESS:-8000} app:app
+gunicorn -w ${WORKER_THREADS:-$(expr $(nproc --all) - 1)} -t ${GUNICORN_TIMEOUT:-180} -b 0.0.0.0:${BIND_ADDRESS:-8000} app:app
